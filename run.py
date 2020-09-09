232366e83dffc34040a129aa6248d399c75fc4dc
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2020-09-09 09:14:58 +0100

    moving outputs to /data

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 from datetime import datetime, timedelta
+import shutil
 
 duration = timedelta(days=int(os.getenv('DURATION', '1')))
 start = datetime(1977, 10, 1)
@@ -13,3 +14,7 @@ with open('cobres-model/input_cob_frd.txt', 'w') as f:
     f.write(text.format(month=end.month, day=end.day))
 
 subprocess.call(['./shetran-linux', '-f', 'cobres-model/rundata_cob.txt'])
+
+for f in os.listdir('cobres-model'):
+    if f.startswith('output'):
+        shutil.move(f'cobres-model/{f}', '/data/outputs')
