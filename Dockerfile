675a7ef0352c8c130cd3c2f7b2d77524e25aeec7
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2020-10-05 10:58:19 +0100

    added shetran-prepare and publishing input files

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,9 @@ RUN mkdir /src
 
 WORKDIR /src
 
-COPY cobres-model cobres-model
-COPY shetran-linux run.py ./
+COPY aire-at-kildwick-bridge aire-at-kildwick-bridge
+COPY shetran/shetran-linux shetran/shetran-prepare run.py ./
+COPY shetran/lib /usr/lib/
 
 RUN chmod +x shetran-linux
 
--- run.py
+++ run.py
@@ -4,17 +4,21 @@ from datetime import datetime, timedelta
 import shutil
 
 duration = timedelta(days=int(os.getenv('DURATION', '1')))
-start = datetime(1977, 10, 1)
+start = datetime(1980, 1, 1)
 end = start + duration
 
-with open('cobres-model/input_cob_frd_template.txt') as f:
+path = 'aire-at-kildwick-bridge'
+
+with open(f'{path}/Aire_at_Kildwick_BridgeLibraryFile_Template.xml') as f:
     text = f.read()
 
-with open('cobres-model/input_cob_frd.txt', 'w') as f:
+library = f'{path}/Aire_at_Kildwick_BridgeLibraryFile.xml'
+with open(library, 'w') as f:
     f.write(text.format(month=end.month, day=end.day))
 
-subprocess.call(['./shetran-linux', '-f', 'cobres-model/rundata_cob.txt'])
+subprocess.call(['./shetran-prepare', library])
+subprocess.call(['./shetran-linux', '-f', f'{path}/rundata_Aire_at_Kildwick_Bridge.txt'])
 
-for f in os.listdir('cobres-model'):
-    if f.startswith('output'):
-        shutil.move(f'cobres-model/{f}', '/data/outputs')
+for f in os.listdir(path):
+    if not any([f.endswith('Template.xml'), f.startswith('input'), f.startswith('rundata'), f == 'temporary.txt']):
+        shutil.move(f'{path}/{f}', f'/data/outputs/{f}')
