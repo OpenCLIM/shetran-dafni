cad3f95b880657217426e64d8f4d8d1654a6f322
Fergus McClean <fergus.mcclean@newcastle.ac.uk>
2021-09-20 14:05:17 +0100

    Move model files to subdirectory

--- run.py
+++ run.py
@@ -10,14 +10,17 @@ outputs = os.path.join(data_path, 'outputs')
 
 if os.path.exists(outputs):
     shutil.rmtree(outputs)
+    os.mkdir(outputs)
 
-shutil.copytree(inputs, outputs)
+run_path = os.path.join(outputs, 'shetran')
+
+shutil.copytree(inputs, run_path)
 
 try:
-    library = glob(os.path.join(outputs, '*.xml'))[0]
+    library = glob(os.path.join(run_path, '*.xml'))[0]
 except IndexError:
     raise Exception('Library file missing')
 
 
 subprocess.call(['./shetran-prepare', library])
-subprocess.call(['./shetran-linux', '-f', glob(os.path.join(outputs, 'rundata_*'))[0]])
+subprocess.call(['./shetran-linux', '-f', glob(os.path.join(run_path, 'rundata_*'))[0]])
